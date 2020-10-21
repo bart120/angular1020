0d3196fd97df0b5d9acd282e05d102948e84a58f
Vincent <vleclerc@inow.fr>
2020-10-21 14:46:59 +0200

    session storage

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { UserModel } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -8,17 +9,29 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user: UserModel = null;
+
   @ViewChild('password')
   private passwd: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(sessionStorage.getItem('USER'));
   }
 
   onLogin(mail: any): void {
     console.log(mail);
     console.log(this.passwd.nativeElement.value);
+    //appel serveur
+
+    this.user = { login: mail };
+    sessionStorage.setItem('USER', JSON.stringify(this.user));
+  }
+
+  onLogout(): void {
+    sessionStorage.removeItem('USER');
+    this.user = null;
   }
 
 }
